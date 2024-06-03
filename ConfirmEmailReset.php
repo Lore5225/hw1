9f60d055d90188b9357c0dc5d5f744d804a1f324
Lore5225 <varsallonallorenzo@gmail.com>
2024-06-03 21:56:14 +0200

    undicesimo commit, tolto codice non in uso

--- ConfirmEmailReset.php
+++ ConfirmEmailReset.php
@@ -8,18 +8,6 @@ if (isset($_GET['status']) && isset($_GET['email']) && $_GET['status'] == 'succe
     $message = "Errore durante l'invio dell'email di reset all'indirizzo " . ($_GET['email']) . " riprova più tardi " ;
 }
 ?>
-<!DOCTYPE html>
-<html lang="it">    
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="confirm.css">
-    <script src="confirm.js"></script>
-    <title>Conferma</title>
-</head>
-<body>
-    <div class="message-box">
-       
 
 
 <!DOCTYPE html>
